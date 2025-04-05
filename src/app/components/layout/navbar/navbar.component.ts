87930bc8ea0136643a8c5846eda8d08ac5f03286
Brambs <gabriel128.br@gmail.com>
2025-04-05 10:35:47 -0300

    Organization and adding libs

---
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MdbCollapseModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  constructor() {
+    // Swal.fire('Hey user!', 'You are the rockstar!', 'info');
+  }
+}
